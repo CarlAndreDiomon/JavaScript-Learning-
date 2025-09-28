2b18c4cc4c2b70403d59ee7a4d8450e8b3fe3237
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2025-09-28 09:27:59 +0800

    Day 1

---
+++ Day1.js
@@ -0,0 +1,9 @@
+const username = 'SoftEn';
+let age = 20;
+const isStudent = true;
+let hobbies =  "Coding, Plaiyng, Singing";
+
+console.log("Name",username);
+console.log("Age:",age);
+console.log("Student:", isStudent);
+console.log("Hobbies:", hobbies );
